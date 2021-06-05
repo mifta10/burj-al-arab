8ba2e22152b946825153b6f78d6c7d82c7d7775b
mifta10 <miftauddinkhan10@gmail.com>
2021-06-05 22:06:23 +0600

    front end added

---
+++ src/components/Login/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useContext } from 'react';
+import {UserContext} from '../../App';
+import { useHistory, useLocation } from 'react-router';
+
+const Login = () => {
+    const [loggedInUser, setloggedInUser] = useContext(UserContext);
+
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                const {displayName, email} = result.user;
+                const signedInUser = {name : displayName, email}
+                setloggedInUser(signedInUser);
+                history.replace(from);
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    }
+    return (
+        <div>
+            <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxnmHa7bghHleV8E5CQ7Aq5jsKVOHB-pg",
+  authDomain: "burj-al-arab-a09c6.firebaseapp.com",
+  projectId: "burj-al-arab-a09c6",
+  storageBucket: "burj-al-arab-a09c6.appspot.com",
+  messagingSenderId: "1058819562852",
+  appId: "1:1058819562852:web:2c25345b0564a9fb396e71"
+};
+
+export default firebaseConfig;
